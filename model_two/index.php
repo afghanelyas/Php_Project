e6db9f68dab24054584dbb9a3f41ceb3af2e52db
elyasafghan <afghan.elyas1@gmail.com>
2023-04-19 14:51:53 +0430

    connect to the database

--- model_two/index.php
+++ model_two/index.php
@@ -5,4 +5,24 @@ ini_set('display_errors', '1');
 
 require "function.php";
 
-require "router.php";
+// require "router.php";
+
+// connect to the database
+
+$dns = "mysql:host=localhost;port=3306;dbname=myapp;user=elyas;password=123@Kabul@!@#;charset=utf8";
+
+$pdo = new PDO($dns);
+
+
+$statement = $pdo->prepare("SELECT * FROM info");
+
+$statement->execute();
+
+
+$result = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($result as $row){
+
+    echo "<li>" . $row['name'] . "</li>";
+
+}
