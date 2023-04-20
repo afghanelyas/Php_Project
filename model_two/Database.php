4ffdd7e44958269c081fab37f99b4700479d3388
elyasafghan <afghan.elyas1@gmail.com>
2023-04-20 09:37:04 +0430

    bring some refactoring in case of config

--- model_two/Database.php
+++ model_two/Database.php
@@ -7,21 +7,28 @@ class Database {
     
     public $connection;
 
-    public function __construct() {
+    
+    public function __construct($config , $username = "elyas", $password = "123@Kabul@!@#") {
 
+       
+        $dsn = 'mysql' . http_build_query($config , '' , ';');
         
-        $dns = "mysql:host=localhost;port=3306;dbname=myapp;user=elyas;password=123@Kabul@!@#;charset=utf8";
         
-        $this->connection = new PDO($dns);
+        $this->connection = new PDO($dsn , $username , $password, [
+
+
+           PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC 
+            
+
+        ]);
 
     }
 
     public function query($query) {
 
 
-        
-        
         $statement = $this->connection->prepare($query);
+
         
         $statement->execute();
         
