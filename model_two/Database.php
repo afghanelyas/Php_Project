114dddd390abeacd00a9e8b34cd83b0467a0a2e0
elyasafghan <afghan.elyas1@gmail.com>
2023-04-25 11:07:19 +0430

    create the prameter in query function

--- model_two/Database.php
+++ model_two/Database.php
@@ -1,17 +1,18 @@
 <?php
 // connect to the database, and execute a query
+
 class Database {
     public $connection;
-    public function __construct($config , $username = "elyas", $password = "123@Kabul@!@#"){
+    public function __construct($config , $username = 'elyas', $password = "123@Kabul@!@#"){
       
         $dsn = 'mysql:' . http_build_query($config , '' , ';');
         $this->connection = new PDO($dsn , $username , $password , [
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         ]);
     }
-    public function query($query){
+    public function query($query , $prams=[]){
         $statment = $this->connection->prepare($query);
-        $statment->execute();
+        $statment->execute($prams);
         return $statment;
     }
 }
