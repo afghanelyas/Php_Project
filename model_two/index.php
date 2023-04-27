a0e1aabb423af4f57318549e75b61e28c4e235fb
afghanelyas <afghan.elyas1@gmail.com>
2023-04-27 16:37:42 +0430

    fixed the authorization

--- model_two/index.php
+++ model_two/index.php
@@ -3,6 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', '1');
 require "function.php";
 require "Database.php";
+require "Response.php";
 require "router.php";
 $config = require"config.php";
 $db = new Database($config['database']);
