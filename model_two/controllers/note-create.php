5d628aed144d8ebae5dec6c2a39f42abc1b42c3b
afghanelyas <afghan.elyas1@gmail.com>
2023-04-29 06:49:27 +0430

    fixed the validation form

--- model_two/controllers/note-create.php
+++ model_two/controllers/note-create.php
@@ -4,10 +4,22 @@ $db = new Database($config['database']);
 $heading = "Create Note";
 
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
-    $db->query("INSERT INTO notes( body , user_id) VALUES ( :body, :user_id)" , [
-        'body' => $_POST['body'],
-        'user_id' => 2
+    
+    $errors = [];
+    if(strlen($_POST['body']) === 0){
+        $errors['body'] = "The body is should be filled";
+    }
 
-    ]);
+    if(strlen($_POST['body']) > 500){
+        $errors['body'] = "The body is too long more than 500 characters";
+    }
+
+    if(empty($errors)){
+        $db->query("INSERT INTO notes( body , user_id) VALUES ( :body, :user_id)" , [
+            'body' => $_POST['body'],
+            'user_id' => 2
+        ]);
+    }
+
 }
 require "views/note-create.view.php"; 
--- model_two/views/note-create.view.php
+++ model_two/views/note-create.view.php
@@ -10,12 +10,22 @@
         <div class="col-span-full">
           <label for="body" class="block text-sm font-medium leading-6 text-gray-900">Body</label>
           <div class="mt-2">
-            <textarea id="body" name="body" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+            <textarea 
+                id="body" 
+                name="body" 
+                rows="3"  
+                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                > <?= isset($_POST['body']) ?? '' ?> </textarea>
+                <?php if(isset($errors['body'])): ?>
+                  <p class="text-red-500 text-xs mt-2"><?= $errors['body'] ?></p>
+                <?php endif;?>
           </div>
         </div>
    
   <div class=" flex items-center gap-x-6">
-    <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+    <button 
+        type="submit" 
+        class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
   </div>
 </form>
 
