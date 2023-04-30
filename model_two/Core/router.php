0984afc7a6a818ead9f7b2b31a1c7632ef8c44e8
afghanelyas <afghan.elyas1@gmail.com>
2023-04-30 20:23:22 +0430

    fixed the issues of Core directory

--- model_two/Core/router.php
+++ model_two/Core/router.php
@@ -1,15 +1,15 @@
 <?php
-$routes = require('routes.php');
+$routes = require base_path('routes.php');
 function routeToController($uri , $routes){
 if (array_key_exists($uri, $routes)){
-        require $routes[$uri];
+        require base_path($routes[$uri]);
     }else{
         abort();
     }
 }
 function abort($code = 404) {
     http_response_code($code);
-    require "views/{$code}.php";
+    require base_path("views/{$code}.php");
     die();
 }
 $uri  = parse_url( $_SERVER['REQUEST_URI'])['path'];
--- model_two/public/index.php
+++ model_two/public/index.php
@@ -3,10 +3,10 @@ error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
 const BASE_PATH = __DIR__.'/../';
-require BASE_PATH ."function.php";
+require BASE_PATH ."Core/function.php";
 
 spl_autoload_register(function ($class){
-    require base_path($class . ".php");
+    require base_path("Core/{$class}.php");
 });
-require base_path("router.php");
+require base_path("Core/router.php");
 
