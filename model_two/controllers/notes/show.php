0353a39a965e607f931ad5738e3181f854827ba1
afghanelyas <afghan.elyas1@gmail.com>
2023-05-03 03:41:06 +0430

    handling multiple requeset and active the delete option

--- model_two/controllers/notes/show.php
+++ model_two/controllers/notes/show.php
@@ -3,14 +3,28 @@
 $config = require base_path("config.php");
 $db = new Database($config['database']);
 
+// request for delect query
 $currentUserId = 2;
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $note = $db->query("SELECT * FROM notes WHERE id = :id" , [
+        'id' => $_GET['id']
+    ])->findOrFail();
+    authorize($note['user_id'] === $currentUserId);
 
-$note = $db->query("SELECT * FROM notes WHERE id = :id" , [
-    'id' => $_GET['id']
-])->findOrFail();
-authorize($note['user_id'] === $currentUserId);
+    $db->query("DELETE FROM notes WHERE id = :id", [
+        'id' => $_GET['id']
+    ]);
+    header("Location: /notes");
+    exit;
+}else{
+    
+    $note = $db->query("SELECT * FROM notes WHERE id = :id" , [
+        'id' => $_GET['id']
+    ])->findOrFail();
+    authorize($note['user_id'] === $currentUserId);
+    view("notes/show.view.php" , [
+        "heading" => "Note",
+        "note" => $note
+    ]); 
+}
 
-view("notes/show.view.php" , [
-    "heading" => "Note",
-    "note" => $note
-]); 
--- model_two/views/notes/show.view.php
+++ model_two/views/notes/show.view.php
@@ -8,6 +8,10 @@
     <a href="/notes" class="text-blue-500 underline">go back...</a>
     </p>
     <p> <?= htmlspecialchars($note['body']) ?></p>
+    <form class=" mt-6 " method="POST">
+      <input type="hidden" name="id" value="<?= $note['id'] ?> "></input>
+      <button class="text-sm text-red-500  ">Delete</button>
+    </form>
     </div>
   </main>
 <?php require base_path('views/partials/footer.php'); ?>
